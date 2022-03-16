20852d535f22bc107f82069e11b1ee410eb8c664
Jonathan Abayie Boahene <jonathanboahene@gmail.com>
2022-03-15 18:06:10 -0700

    Adding natural numbers file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+#include <main.h>
+
 /**
- * main - Entry point
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
+	int a, b;
 
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
 	return (0);
 }
