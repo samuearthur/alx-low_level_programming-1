e732d80cb67886c504f9c9aa427b3049988cc4cd
Jonathan Abayie Boahene <jonathanboahene@gmail.com>
2022-03-22 19:10:01 -0700

    Added print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
-* more_numbers - prints 10 times the numbers, from 0 to 14
-* followed by a new line
+* print_line - prints a line of _ n long
+* @n: length of line
+*
+* Return: void
 */
-void more_numbers(void)
-{
-	int i, j;
 
-	for (i = 0; i < 10; i++)
+void print_line(int n)
+{
+	int line = 0;
+
+	while (line < n)
 	{
-		for (j = 0; j < 15; j++)
-		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
-		}
-		_putchar('\n');
+		_putchar('_');
+		line++;
 	}
+	_putchar('\n');
 }
